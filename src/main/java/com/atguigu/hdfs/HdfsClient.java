4a86276293449a961644c48710cae3cc244968b8
AllenYale <137728688@qq.com>
2023-06-04 22:40:49 +0800

    hdfs api操作

--- src/main/java/com/atguigu/hdfs/HdfsClient.java
+++ src/main/java/com/atguigu/hdfs/HdfsClient.java
@@ -1,8 +1,7 @@
 package com.atguigu.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +9,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 
 /**
  * ClassName: HdfsClient
@@ -46,6 +46,7 @@ public class HdfsClient {
         //获取客户端对象
         fileSystem = FileSystem.get(uri, configuration, user);
     }
+
     @After
     public void close() throws IOException {
         //关闭资源
@@ -70,13 +71,75 @@ public class HdfsClient {
         fileSystem.copyFromLocalFile(false, true, new Path("F:\\sunwukong.txt"), new Path("hdfs://hadoop102/xiyou/huaguoshan"));
     }
 
+    /**
+     * 文件下载
+     * 参数解析：useRawLocalFileSystem 是否使用CRC校验
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testGet() throws IOException {
+        fileSystem.copyToLocalFile(false, new Path("hdfs://hadoop102/xiyou/huaguoshan"), new Path("F:\\test_download_upload"), false);
+    }
 
+    //删除
+    @Test
+    public void testRm() throws IOException {
+        //删除文件
+        fileSystem.delete(new Path("/jdk-8u212-linux-x64.tar.gz"), false);
 
+        //删除非空目录，必须递归删除
+        fileSystem.delete(new Path("hdfs://hadoop102/xiyou"), true);
 
+        //删除空目录，不用递归删除
+//        fileSystem.delete(new Path("hdfs://hadoop102/jinguo"), true);
+    }
 
+    //文件更名、移动
+    @Test
+    public void testmv() throws IOException {
+        fileSystem.rename(new Path("/input/word.txt"), new Path("/cls.txt"));
 
+        //目录更名
+        fileSystem.rename(new Path("/input"), new Path("/output"));
+    }
 
+    //获取文件详情信息
+    @Test
+    public void fileDetail() throws IOException {
+        RemoteIterator<LocatedFileStatus> listFiles = fileSystem.listFiles(new Path("/"), true);
 
+        while (listFiles.hasNext()) {
+            LocatedFileStatus fileStatus = listFiles.next();
+            System.out.println("================" + fileStatus.getPath() + "================");
+            System.out.println(fileStatus.getPermission());
+            System.out.println(fileStatus.getOwner());
+            System.out.println(fileStatus.getGroup());
+            System.out.println(fileStatus.getLen());
+            System.out.println(fileStatus.getModificationTime());
+            System.out.println(fileStatus.getReplication());
+            System.out.println(fileStatus.getBlockSize());
+            System.out.println(fileStatus.getPath().getName());
+
+            //获取块存储位置
+            BlockLocation[] blockLocations = fileStatus.getBlockLocations();
+            Arrays.toString(blockLocations);
+        }
+    }
+
+    @Test
+    public void isFileOrDir() throws IOException {
+        FileStatus[] fileStatuses = fileSystem.listStatus(new Path("/"));
+
+        for (FileStatus status : fileStatuses) {
+            if(status.isFile()){
+                String name = status.getPath().getName();
+                System.out.println("文件：" + name);
+            }else {
+                System.out.println("目录：" + status.getPath().getName());
+            }
+        }
+    }
 
 
 }
